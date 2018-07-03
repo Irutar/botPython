cec6de33d58370dd7a27028c69ca05f5856241c0
irutar <krystian_galazka@wp.pl>
2018-07-04 00:23:15 +0200

    Config file

--- config.py
+++ config.py
@@ -1,8 +1,18 @@
-"""screen configuration"""
+"""screen capture configuration"""
 GRAY_VALUE = True
+IMAGE_SAFE_BUFFOR = 10
+
+
+"""Delay values"""
+DELAY_IN_MOVEMENT = 2
+DELAY_FOR_LOAD = 5
+
+
+"""Debug configruation"""
+GET_EXECUTION_TIME_INFO = False
 
 
 """Values that are changed by program"""
 resource_icon_coordinates = 0
-
-
+a_icon_coordinates = 0
+b_icon_coordinates = 0
